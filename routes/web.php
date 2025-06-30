b98a688935e64fc3cfef88f7802cb40e0b073104
Epangssss <edwardlpramudya2005@gmail.com>
2025-07-01 00:06:02 +0700

    "epang"

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/beranda', function () {
-    return view('landingpage');
-});
 
+Route::get('/', fn() => redirect()->route('dashboard'));
+
+Route::get('/Dashboard', fn() => view('landingpage'))->name('dashboard');
