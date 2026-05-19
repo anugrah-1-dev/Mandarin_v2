d382f637370bacde2fd1a0007d2f3a3635b420fc
anugrah-1-dev <tejomaliki1@gmail.com>
2026-05-19 19:29:47 +0700

    fix: add validation error display in laundry create/edit forms

--- resources/views/admin/laundry/create.blade.php
+++ resources/views/admin/laundry/create.blade.php
@@ -17,6 +17,16 @@
                     <form action="{{ route('admin.laundry.store') }}" method="POST" enctype="multipart/form-data" id="laundryForm">
                         @csrf
 
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul class="mb-0">
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
                         <div class="row">
                             <!-- Kolom Kiri -->
                             <div class="col-md-6">
--- resources/views/admin/laundry/edit.blade.php
+++ resources/views/admin/laundry/edit.blade.php
@@ -20,6 +20,16 @@
                             @csrf
                             @method('PUT')
 
+                            @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul class="mb-0">
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="card card-warning card-outline">
